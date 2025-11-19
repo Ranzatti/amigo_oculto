e9b132f607264c6ecc3c9d5315efd28f9115858e
Ranzatti <ranzatti@sonner.com.br>
2025-11-19 17:22:04 -0300

    Ajuste de 2025

--- amigo_secreto.py
+++ amigo_secreto.py
@@ -7,8 +7,12 @@ import pyautogui
 import schedule
 from urllib import parse
 
-# import tkinter as tk
+import tkinter as tk
 
+POSICAO_X_MOUSE_INICIAL = 1200  # na horizontal
+POSICAO_X_MOUSE_FINAL = 2350  # na horizontal
+POSICAO_Y_MOUSE_INICIAL = 0  # na vertical
+POSICAO_Y_MOUSE_FINAL = 980  # na vertical
 
 def amigo_secreto(participantes):
     random.shuffle(participantes)
@@ -19,9 +23,9 @@ def amigo_secreto(participantes):
 
 def enviar_zap(participantes):
     for participante in participantes:
-        pyautogui.click(1200, 0)
+        pyautogui.click(POSICAO_X_MOUSE_INICIAL, POSICAO_Y_MOUSE_INICIAL)
 
-        # espera 60 segundos para enviar a nova mensagem
+        # espera 60 segundos para abrir a pagina e enviar a nova mensagem
         time.sleep(60)
 
         mensagem = f"""
@@ -37,7 +41,7 @@ def enviar_zap(participantes):
 
         Agora que o segredo foi revelado, é hora de começar a pensar no presente perfeito para surpreender o seu amigo. 
         
-        Lembre-se de que o valor sugerido para o presente é de _*R$60,00*_, mas o mais importante é a criatividade e o carinho envolvidos no gesto.
+        Lembre-se de que o valor sugerido para o presente é de _*R$70,00*_, mas o mais importante é a criatividade e o carinho envolvidos no gesto.
         
         Prepare-se para uma noite cheia de risadas, alegria e, é claro, presentes incríveis!
 
@@ -51,56 +55,59 @@ def enviar_zap(participantes):
         web.open("https://web.whatsapp.com/send?phone=+55" + participante['telefone'] + "&text=" + texto)
 
         # Posiciona o cursor no botão de envio
-        pyautogui.click(2040, 980)
+        pyautogui.click(POSICAO_X_MOUSE_FINAL, POSICAO_Y_MOUSE_FINAL)
 
         # espera X segundos para clicar no botão de envio
-        time.sleep(30)
+        time.sleep(10)
 
         # clica no botão de enviar do zap web
-        k.press_and_release('enter')
+        # k.press_and_release('enter')
+        pyautogui.press('enter')
 
         print('Enviado com sucesso para: ', participante['nome'])
 
 
-def enviar_lista_ana(participantes):
-    pyautogui.click(1200, 0)
+def enviar_lista_contemplados(participantes):
+    # essa etapa envia o arquivo de todos os sorteados para uma pessoa como backup e possivel conferencia
+    pyautogui.click(POSICAO_X_MOUSE_INICIAL, POSICAO_Y_MOUSE_INICIAL)
     time.sleep(60)
 
-    mensagem = f"🎅Sorteados do Amigo Oculto 2024🎅\n\n" + "\n".join(
+    mensagem = f"🎅Sorteados do Amigo Oculto 2025🎅\n\n" + "\n".join(
         [f"{item['nome']} → {item['sorteado']}" for item in participantes])
 
     texto = urllib.parse.quote(mensagem)
 
     # abre a pagina de whatsapp web
-    web.open("https://web.whatsapp.com/send?phone=+5534998723109&text=" + texto)
+    web.open("https://web.whatsapp.com/send?phone=+5534998100025&text=" + texto)
 
     # Posiciona o cursor no botão de envio
-    pyautogui.click(2040, 980)
+    pyautogui.click(POSICAO_X_MOUSE_FINAL, POSICAO_Y_MOUSE_FINAL)
 
     # espera X segundos para clicar no botão de envio
     time.sleep(30)
 
     # clica no botão de enviar do zap web
-    k.press_and_release('enter')
+    # k.press_and_release('enter')
+    pyautogui.press('enter')
 
-    pyautogui.click(1200, 0)
+    pyautogui.click(POSICAO_X_MOUSE_INICIAL, POSICAO_Y_MOUSE_INICIAL)
 
     print('Enviado Lista dos parcipantes pra Ana com sucesso!')
 
 
-def criar_arquivo(participantes):
+def criar_arquivo_contemplados(participantes):
     nome_arquivo = "sorteados.txt"
 
     # Criando o arquivo e escrevendo os itens da lista
     with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
-        arquivo.write(f"🎅 Sorteados do Amigo Oculto 2024 🎅\n\n" + "\n".join(
+        arquivo.write(f"🎅 Sorteados do Amigo Oculto 2025 🎅\n\n" + "\n".join(
             [f"{item['nome']} → {item['sorteado']}" for item in participantes]) + "\n")
 
     print(f"Arquivo '{nome_arquivo}' criado com sucesso!")
 
 
 def gerar_arquivo():
-    # Obtém a largura e a altura da tela
+    # essas linhas comentadas serve para vc ver qual o tamanho da sua tela para configurar a posição do mouse - obtém a largura e a altura da tela
     # root = tk.Tk()
     # largura_tela = root.winfo_screenwidth()
     # altura_tela = root.winfo_screenheight()
@@ -110,28 +117,43 @@ def gerar_arquivo():
 
     print('Gerando Arquivo...')
 
+    # lista_participantes = {
+    #     'Laura': '34996690025',
+    #     'Raquel': '34999830025',
+    #     'Matheus': '34998089988',
+    #     'Ricardo': '34998100025',
+    #     'Ana': '34998723109',
+    #     'James': '34984438013',
+    # }
+
     lista_participantes = {
-        'Laura': '34996690025',
-        'Raquel': '34999830025',
-        'Matheus': '34998089988',
+        'Cecília': '61991743074',
+        'James': '34984438013',
         'Ricardo': '34998100025',
-        'Fernanda': '61992856117',
-        'Cecilia': '61992856117',
-        'Milania': '61996993610',
+        'Vovó Nene': '34991733739',
+        'Marcus Vinícius': '61982055633',
+        'Vovô Mário': '34991733739',
+        'Milânia': '61996993610',
         'Marco Aurélio': '61992164119',
-        'Maria Paula': '61998410397',
-        'Alba': '34991733739',
-        'Galeno': '34997259024',
         'Joyce': '62998651781',
-        'Ariane': '67981584772',
-        'Marlei': '61991354006',
+        'Fernanda': '61992856117',
+        'Raquel': '34999830025',
+        'Guilherme': '67992641716',
+        'Laura': '34996690025',
+        'Joelma': '62985889078',
         'João Gabriel': '61999198248',
-        'Alice': '34988614573',
-        'Fred': '34996794554',
-        'Mário': '34988614573',
-        'Marlene': '34988614573',
-        'Humilda': '34999796471',
-        'Cirlene': '34991141424',
+        'Marlei': '61991354006',
+        'Maria Paula': '61998410397',
+        'Eduardo': '67993090917',
+        'Beatriz': '11952998748',
+        'Mariana': '67993244203',
+        'Maria Eduarda': '67993244203',
+        'Ariane': '67981584772',
+        'Galeno': '34997259024',
+        'Ana Luiza': '34998723109',
+        'Alba': '34991733739',
+        'Peixoto': '61995204803',
+        'Matheus': '34998089988'
     }
 
     participantes = []
@@ -163,9 +185,9 @@ def gerar_arquivo():
         for nome in nomes:
             lista.append({"nome": nome[0], "telefone": lista_participantes.get(nome[0]), "sorteado": nome[1]})
 
-        criar_arquivo(lista)
+        criar_arquivo_contemplados(lista)
         enviar_zap(lista)
-        enviar_lista_ana(lista)
+        enviar_lista_contemplados(lista)
 
         print('Sorteio realizado com Sucesso!')
         exit()
